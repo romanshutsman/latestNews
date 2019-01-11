9d79be72b0f8ab60df56d3cbf7cac97034fd506a
romanshutsman <roman13275@gmail.com>
2019-01-11 13:40:29 +0200

    implemented a new APIs

--- helpers/controller/apiHandler.js
+++ helpers/controller/apiHandler.js
@@ -1,11 +1,14 @@
 const models = require('./../models');
 const db = require('mongoose');
 const News = db.model('News');
+const request = require('request');
+
+const URL_exch_curr = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 
 module.exports = {
     getAllNews: async (req, res) => {
         try {
-            const foundDoc = await News.find({}, (err, doc) => doc)
+            const foundDoc = await News.find({}, {}, {limit: 3, sort: {date: -1}}, (err, doc) => doc)
             if (foundDoc) return res.send(foundDoc)
         } catch (e) {
             res.send(e.message);
@@ -22,14 +25,14 @@ module.exports = {
     createNews: async (req, res) => {
         try {
             const foundDoc = await News.find({ description: req.body.description }, (err, doc) => doc)
-            if (foundDoc.length > 0) return res.send('Description should be unique!')
+            if (foundDoc.length > 0) return res.status(400).send('Description should be unique!')
         } catch (e) {
             return res.send(e.message)
         }
 
         try {
             const foundDoc = await News.find({ fullDescription: req.body.fullDescription }, (err, doc) => doc)
-            if (foundDoc.length > 0) return res.send('Full description should be unique!')
+            if (foundDoc.length > 0) return res.status(400).send('Full description should be unique!')
         } catch (e) {
             return res.send(e.message)
         }
@@ -47,7 +50,7 @@ module.exports = {
     },
     editNews: async (req, res) => {
         const id = req.body._id;
-        if (!id) return res.send('Id is required!')
+        if (!id) return res.status(400).send('Id is required!')
         const updatedItem = req.body;
         updatedItem['edited'] = true;
         updatedItem['publishedAt'] = Date.now();
@@ -63,14 +66,43 @@ module.exports = {
     },
     deleteNews: async (req, res) => {
         const id = req.params.news_id;
-        if(!id) return res.send('Id is required!');
-        
+        if (!id) return res.status(400).send('Id is required!');
+
         try {
             return await News.findOneAndRemove({ _id: id }, (err, doc) => {
-                console.log(doc);
                 if (err) return res.send(err.message)
                 if (doc) return res.send('Successfully deleted!');
             })
+        } catch (e) {
+            return res.send(e.message)
+        }
+    },
+    getCurrency: async (req, res) => {
+        try {
+            await request(URL_exch_curr, (err, response, body) => {
+                if(body) return res.send(JSON.parse(body));
+            });
+
+        } catch (e) {
+            return res.send(e.message);
+        }
+    },
+    getNewsByCategory: async (req, res) => {
+        try {
+            const foundDoc = await News.find({category: req.params.category}, (err, doc) => doc)
+            if(foundDoc.length == 0) return res.status(400).send('News by category not found!');
+            if(foundDoc) return res.send(foundDoc);
+        } catch (e) {
+            return res.send(e.message)
+        }
+    },
+    getNewsByPage: async (req, res) => {
+        const page = parseInt(req.params.page);
+        if(page === 0) return res.status(400).send('Invalid page number, should start with 1!');
+        try {
+            const reqPage = await News.find({}, {}, {limit: 3, skip: 3*(page-1)}, (err, doc) => doc)
+            if(reqPage.length == 0) return res.status(400).send('Invalid page number!');
+            if(reqPage) return res.send(reqPage);
         } catch (e) {
             return res.send(e.message)
         }
--- helpers/routes/index.js
+++ helpers/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const editor = require('./../controller/editor');
+const apiHandler = require('./../controller/apiHandler');
 
 module.exports = app => {
-    router.get('/api/news', editor.getAllNews);
-    router.get('/api/news/:news_id', editor.getNewsById);
-    router.post('/api/news', editor.createNews);
-    router.patch('/api/news', editor.editNews);
-    router.delete('/api/news/:news_id', editor.deleteNews);
+    //API News
+    router.get('/api/news', apiHandler.getAllNews);
+    router.get('/api/news/:news_id', apiHandler.getNewsById);
+    router.post('/api/news', apiHandler.createNews);
+    router.patch('/api/news', apiHandler.editNews);
+    router.delete('/api/news/:news_id', apiHandler.deleteNews);
+    
+    router.get('/api/news/category/:category', apiHandler.getNewsByCategory);
+    router.get('/api/news/page/:page', apiHandler.getNewsByPage);
+
+    router.get('/api/get-currency', apiHandler.getCurrency);
     app.use(router);
 }
